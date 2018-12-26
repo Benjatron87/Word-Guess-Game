f8c3ec190c0812656bc061a41678dd5e6d72a631
Benjatron <benjt122@gmail.com>
2018-12-25 22:51:29 -0400

    Added a few minor changes

--- javascript/game.js
+++ javascript/game.js
@@ -38,6 +38,9 @@ document.body.onkeyup = function(e){
         // Prompt the user as to what to do next.
         document.getElementById("keys").innerHTML = "Press any Letter to Guess!";
 
+        // Sets background color.
+        document.getElementById("body").style.backgroundColor = "whitesmoke";
+
         // State variables to link to HTML.
         var DOMblanks = document.getElementById("blanks");
 
@@ -78,88 +81,93 @@ document.body.onkeyup = function(e){
             // When any key is pressed it acts as a guess and the following processes are run.
             document.onkeypress = function(event) {
 
-                // As long as there are more guesses left and the word has not been guessed in full do the following.
-                if(guessesLeft > 0 && (blanksWord !== lettersWord)){
-                
-                    document.getElementById("guesses").innerHTML = guessesLeft;
+                    // As long as there are more guesses left and the word has not been guessed in full do the following.
+                    if(guessesLeft > 0 && (blanksWord !== lettersWord)){
+                    
+                        document.getElementById("guesses").innerHTML = guessesLeft;
 
-                    console.log(guessesLeft);
+                        console.log(guessesLeft);
 
-                    // Figures out which key was pressed and adds that to the "guess" variable.
-                    event = event || window.event;
+                        // Figures out which key was pressed and adds that to the "guess" variable.
+                        event = event || window.event;
 
-                    var charCode = event.keyCode || event.which;
+                        var charCode = event.keyCode || event.which;
 
-                    var guess = String.fromCharCode(charCode);
+                        var guess = String.fromCharCode(charCode);
 
-                    guess = guess.toUpperCase();
+                        guess = guess.toUpperCase();
 
-                    var guessRight = false;
+                        var guessRight = false;
 
-                    // Checks if the guess matches any letter in the "letters" array.
-                    for (var i = 0; i < letters.length; i++){
+                        // Checks if the guess matches any letter in the "letters" array.
+                        for (var i = 0; i < letters.length; i++){
 
-                        // if the guess matches it turns that element in the "blanks" array to the guessed letter.
-                        if (guess == letters[i]){
+                            // if the guess matches it turns that element in the "blanks" array to the guessed letter.
+                            if (guess == letters[i]){
 
-                            guessRight = true;
-                            blanks[i] = guess;
+                                guessRight = true;
+                                blanks[i] = guess;
+                            }
+                            blankVar = blankVar + blanks[i];
                         }
-                        blankVar = blankVar + blanks[i];
-                    }
 
-                    // Adds the guessed letter to the HTML when a correct guess takes place.
-                    document.getElementById("blanks").innerHTML = blankVar;
+                        // Adds the guessed letter to the HTML when a correct guess takes place.
+                        document.getElementById("blanks").innerHTML = blankVar;
 
-                    console.log(guessNum);
-                    
-                    // Returns this variable to its blank state to ready it for the next guess.
-                    blankVar = "";
-
-                    console.log(blanks);
-                    console.log(letters);
-
-                    // Turns the "blanks" and "letters" arrays into strings that can be compared with each other.
-                    blanksWord = blanks.join("");
-                    lettersWord = letters.join("");
-
-                    console.log(blanksWord);
-                    console.log(lettersWord);
-                    
-                    // If the strings "blanksWord" and "lettersWord" match, then "You Win!" is displayed in the HTML.
-                    if (blanksWord == lettersWord){
-
-                        document.getElementById("endgame").style.color = "rgb(0, 162, 255)";
-
-                        document.getElementById("endgame").innerHTML = "You Win!";
-
-                        document.getElementById("keys").innerHTML = "";
-                    }
-
-                    // If your guess was wrong, it adds the letter guessed to the HTML and removes 1 from the guesses left.
-                    if (guessRight == false){
-
-                        guessesLeft--;
-
-                        document.getElementById("wrong-guess").innerHTML += guess;
-                    }
-
-                    // If your number of guesses goes below 1, then it displays "You Lose!" to the HTML.
-                    document.getElementById("guesses").innerHTML = guessesLeft;
-
-                    if(guessesLeft < 1){
+                        console.log(guessNum);
                         
-                        document.getElementById("endgame").innerHTML = "You Lose!";
+                        // Returns this variable to its blank state to ready it for the next guess.
+                        blankVar = "";
 
-                        document.getElementById("endgame").style.color = "red";
+                        console.log(blanks);
+                        console.log(letters);
 
-                        document.getElementById("keys").innerHTML = "";
+                        // Turns the "blanks" and "letters" arrays into strings that can be compared with each other.
+                        blanksWord = blanks.join("");
+                        lettersWord = letters.join("");
 
-                        document.getElementById("blanks").innerHTML = lettersWord;
+                        console.log(blanksWord);
+                        console.log(lettersWord);
+                        
+                        // If the strings "blanksWord" and "lettersWord" match, then "You Win!" is displayed in the HTML.
+                        if (blanksWord == lettersWord){
 
+                            document.getElementById("endgame").style.color = "yellowgreen";
+
+                            document.getElementById("body").style.backgroundColor = "yellowgreen";
+
+                            document.getElementById("endgame").innerHTML = "You Win!";
+
+                            document.getElementById("keys").innerHTML = "Press Space to Start a New Game";
+                        }
+
+                        // If your guess was wrong, it adds the letter guessed to the HTML and removes 1 from the guesses left.
+                        if (guessRight == false){
+
+                            guessesLeft--;
+
+                            document.getElementById("wrong-guess").innerHTML += guess;
+                        }
+
+                        // If your number of guesses goes below 1, then it displays "You Lose!" to the HTML.
+                        document.getElementById("guesses").innerHTML = guessesLeft;
+
+                        if(guessesLeft < 1){
+                            
+                            document.getElementById("endgame").innerHTML = "You Lose!";
+
+                            document.getElementById("endgame").style.color = "red";
+
+                            document.getElementById("body").style.backgroundColor = "red";
+
+                            document.getElementById("keys").innerHTML = "Press Space to Start a New Game";
+
+                            document.getElementById("blanks").innerHTML = lettersWord;
+
+                    }
                 }
 
             }
         }
-    }
+    
     };
